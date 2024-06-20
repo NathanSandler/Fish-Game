ce91f8ff506736c1f1b698be51ccc99ffd6b37b9
NathanSandler <natsandler2@gmail.com>
2024-06-20 17:59:09 -0400

    Portal damage and turret imports

--- Assets/Scripts/Systems/PortalSystem.cs
+++ Assets/Scripts/Systems/PortalSystem.cs
@@ -5,6 +5,7 @@ using Unity.Burst;
 using Unity.Burst.Intrinsics;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
 using Unity.Transforms;
 using UnityEngine;
 
@@ -13,6 +14,8 @@ public partial struct PortalSystem : ISystem
     private EntityQuery _query;
     private EntityTypeHandle _entityHandle;
     private ComponentTypeHandle<LocalToWorld> componentHandle;
+    private NativeQueue<uint> portalQueue;
+    private NativeReference<uint> portalReference;
     
     [BurstCompile]
     public void OnCreate(ref SystemState state)
@@ -20,14 +23,27 @@ public partial struct PortalSystem : ISystem
         _query = SystemAPI.QueryBuilder().WithAll<LocalToWorld, FishComponent>().Build();
         _entityHandle = state.GetEntityTypeHandle();
         componentHandle = state.GetComponentTypeHandle<LocalToWorld>();
+
+        portalQueue = new NativeQueue<uint>(Allocator.Persistent);
+        portalReference = new NativeReference<uint>(Allocator.Persistent);
         
         state.RequireForUpdate<PortalComponent>();
         state.RequireForUpdate<BeginSimulationEntityCommandBufferSystem.Singleton>();
+    }
+    
+    [BurstCompile]
+    public void OnDestroy(ref SystemState state)
+    {
+        portalQueue.Dispose();
+        portalReference.Dispose();
     }
 
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
+        portalQueue.Clear();
+        portalReference.Value = 0;
+        
         EntityCommandBuffer ecb = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>()
             .CreateCommandBuffer(state.WorldUnmanaged);
         
@@ -42,12 +58,37 @@ public partial struct PortalSystem : ISystem
             ecb = ecb.AsParallelWriter(),
             position = pos,
             entityHandle = _entityHandle,
-            componentHandle = componentHandle
+            componentHandle = componentHandle,
+            PortalQueue = portalQueue.AsParallelWriter()
         }.ScheduleParallel(_query, state.Dependency);
 
-        foreach (var (health,_) in SystemAPI.Query<RefRW<HealthComponent>, RefRO<PortalComponent>>())
+        state.Dependency = new PortalQueueJob()
         {
-            health.ValueRW.Health -= SystemAPI.Time.DeltaTime;
+            portalQueue = portalQueue,
+            portalReference = portalReference
+        }.Schedule(state.Dependency);
+        
+        state.Dependency.Complete();
+
+        Entity portalEntity = SystemAPI.GetSingletonEntity<PortalComponent>();
+        HealthComponent healthComponent = SystemAPI.GetComponent<HealthComponent>(portalEntity);
+        healthComponent.Health -= portalReference.Value;
+        SystemAPI.SetComponent(portalEntity, healthComponent);
+    }
+
+    [BurstCompile]
+    public struct PortalQueueJob : IJob
+    {
+        public NativeQueue<uint> portalQueue;
+        public NativeReference<uint> portalReference;
+        
+        [BurstCompile]
+        public void Execute()
+        {
+            while (portalQueue.TryDequeue(out uint damaged))
+            {
+                portalReference.Value += damaged;
+            }
         }
     }
 
@@ -58,19 +99,29 @@ public partial struct PortalSystem : ISystem
         [ReadOnly] public EntityTypeHandle entityHandle;
         [ReadOnly] public ComponentTypeHandle<LocalToWorld> componentHandle;
         public float position;
+
+        public NativeQueue<uint>.ParallelWriter PortalQueue;
         
         [BurstCompile]
         public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask)
         {
             NativeArray<LocalToWorld> positionComponents = chunk.GetNativeArray(ref componentHandle);
             NativeArray<Entity> entities = chunk.GetNativeArray(entityHandle);
+            uint damageTaken = 0;
+            
             for (int i = 0; i < chunk.Count; i++)
             {
                 if (positionComponents[i].Position.z > position)
                 {
                     // TODO: Damage health
+                    damageTaken += 1;
                     ecb.DestroyEntity(unfilteredChunkIndex, entities[i]);
                 }
+            }
+
+            if (damageTaken > 0)
+            {
+                PortalQueue.Enqueue(damageTaken);
             }
         }
     }
